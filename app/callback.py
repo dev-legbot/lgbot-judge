0c8b263729e6fd898bf1054136a0b7a35fdfb23f
ryuta <ryuta.hara@topgate.co.jp>
2018-03-01 14:12:37 +0900

    Add: サイト判定機能作成

--- app/callback.py
+++ app/callback.py
@@ -23,8 +23,26 @@ class Callback(object):
             self._logger.error("Some error raises : %s", ex)
 
     def label_of(self, dom):
-        """タグ使用割合をもとにラベルを返す"""
-        return "old"
+        """Labeling site
+
+        Args:
+            dom(list): list of dom dictionary.
+
+        Returns:
+            lebal of dom.
+             "old" or "modern"
+        """
+        # XXX 特定のタグの利用率のみでサイトタイプの判定をしている。
+        #   * データ観察をして条件をチューニング
+        #   * イケてるサイトの判定処理を加えて、 "どの程度イケてるか", "どの程度ダサいか" を割合で出す
+        # ここらへんの修正が今後必要なはず。
+        # また、将来的には機械学習で判定を行いたい。
+        tag_cnt = sum([d["count"] for d in dom])
+        judged_tag_counts = self._extract_tags_from_dom(
+            dom=dom, tags=config.TAGS_FOR_JUDGE
+        )
+        judged_tag_sum = sum([d["count"] for d in judged_tag_counts])
+        return "old" if judged_tag_sum / tag_cnt >= config.TAG_USE_RATE_FOR_OLD_SITE else "modern"
 
     def parse(self, data):
         """Parse json string to ReceiveMessage object
@@ -45,9 +63,13 @@ class Callback(object):
             url(str): Site url.
             label(str): Site label.
         """
-        self._pubsub_client.publish(config.TOPIC, url, attribute={"label": label})
+        self._pubsub_client.publish(
+            config.TOPIC, url, attribute={"label": label})
 
     def store_to_bigquery(self, url, label):
         """BigQueryに判定結果を保存する"""
         # XXX BigQuery client shoud be inject by constructor.
         pass
+
+    def _extract_tags_from_dom(self, dom, tags):
+        return list(filter(lambda d: d["name"] in tags, dom))
