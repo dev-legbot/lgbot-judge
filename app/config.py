b4c238c3f1fa672fa38dbaa506e82e67b049de9e
ryutah <ryuta0221@gmail.com>
2018-03-09 16:12:41 +0900

    Update: BigQuery設定を環境変数から取得するように変更

--- app/config.py
+++ app/config.py
@@ -2,11 +2,15 @@ import os
 
 
 PROJECT_ID = os.environ["PROJECT_ID"]
-_TOPIC_NAME = os.environ["TOPIC_NAME"]
-_SUBSCRIPTION_NAME = os.environ["SUBSCRIPTION_NAME"]
-TOPIC = "projects/{}/topics/{}".format(PROJECT_ID, _TOPIC_NAME)
+
+TOPIC = "projects/{}/topics/{}".format(PROJECT_ID, os.environ["TOPIC_NAME"])
 SUBSCRIPTION = "projects/{}/subscriptions/{}".format(
-    PROJECT_ID, _SUBSCRIPTION_NAME)
+    PROJECT_ID, os.environ["SUBSCRIPTION_NAME"]
+)
+
+BQ_DATASET = os.environ["BQ_DATASET"]
+BQ_TABLE = os.environ["BQ_TABLE"]
+
 
 # サイト判定に使用するタグのリストと使用割合。
 # 使用割合は、とりあえずサンプルサイトが全て判定可能なギリギリの値を設定
--- app/injector.py
+++ app/injector.py
@@ -1,3 +1,5 @@
+import config
+
 from google.cloud import bigquery
 from google.cloud import pubsub
 
@@ -31,7 +33,7 @@ class Injector(object):
 
     def __init_bigquery(self):
         self.__bigquery_client = bigquery.Client()
-        dataset_ref = self.__bigquery_client.Dataset("")
+        dataset_ref = self.__bigquery_client.Dataset(config.BQ_DATASET)
         dataset = bigquery.Dataset(dataset_ref)
-        table_ref = dataset.table("")
+        table_ref = dataset.table(config.BQ_TABLE)
         self.__table = bigquery.Table(table_ref)
